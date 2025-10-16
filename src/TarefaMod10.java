9029c33beb3034f23535f34bd3798b12aba8d6a5
Deivid Moises <deividmoisesdasilva2017@gmail.com>
2025-10-16 00:41:24 -0300

    Ajuste da tarefa

--- src/TarefaMod10.java
+++ src/TarefaMod10.java
@@ -5,16 +5,25 @@ public class TarefaMod10 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Digite sua nota: ");
-        int nota = s.nextInt();
-        String notaFD = getNota(nota);
-        System.out.println(notaFD );
+        System.out.println("Digite sua nota 1: ");
+        double nota1 =s.nextDouble();
+        System.out.println("Digite sua nota 2: ");
+        double nota2 =s.nextDouble();
+        System.out.println("Digite sua nota 3: ");
+        double nota3 =s.nextDouble();
+        System.out.println("Digite sua nota 4: ");
+        double nota4 =s.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        String resultado = getNota(media);
+        System.out.println("Sua média final é: " + media);
+        System.out.println(resultado);
     }
 
-    public static String getNota(int nota) {
-        if (nota >= 7 ) {
+    public static String getNota(double media) {
+        if (media>= 7 ) {
             return "Você está aprovado";
-        } else if (nota>= 5) {
+        } else if (media>= 5) {
             return "Você está de recuperação";
         }  else {
             return "Você está reprovado, estude mais no proximo semestre!";
